8fa119c3e875c8665384b7e5d847cf03ff7270de
well <waynesun1990@gmail.com>
2020-10-28 11:42:54 +0800

    Init project.

---
+++ Shu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shu
+//
+//  Created by sun well on 2020/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shu/ShuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuApp.swift
+//  Shu
+//
+//  Created by sun well on 2020/10/28.
+//
+
+import SwiftUI
+
+@main
+struct ShuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
